c3dc04f277ec2435f6f38ca56a4cae3193547335
Inpyo-Hong <64252911+Inpyo-Hong@users.noreply.github.com>
2020-10-12 21:59:41 +0900

    model2

--- hakertone/app/admin.py
+++ hakertone/app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User_info, Company, Group_buying, Group_buying_comment, Flee_market
+from .models import User_info, Company, Group_buying, Group_buying_comment, Flee_market, Review
 # Register your models here.
 admin.site.register(User_info)
 admin.site.register(Company)
 admin.site.register(Group_buying)
 admin.site.register(Group_buying_comment)
 admin.site.register(Flee_market)
+admin.site.register(Review)
