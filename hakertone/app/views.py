8dd658c0e8603520fa93f3779d9ca099eb992cae
ToA <nansh9815@naver.com>
2020-10-25 17:13:57 +0900

    finish

--- hakertone/app/views.py
+++ hakertone/app/views.py
@@ -58,7 +58,7 @@ def main(request):
         num=[]
         for i in group_buying:
             num+=[i.id]        
-        randomNum = random.sample(num , 4)        
+        randomNum = random.sample(num , 5)        
         randomObjList=[]
         for i in randomNum:
             randomObjList += Group_buying.objects.filter(id=i)
@@ -67,7 +67,7 @@ def main(request):
         num=[]
         for i in company_buying:
             num+=[i.id]       
-        randomNum1 = random.sample(num , 4)
+        randomNum1 = random.sample(num , 5)
         randomObjList1=[]
         for i in randomNum1:
             randomObjList1.append(list(Company_buying.objects.filter(id=i)))
@@ -126,7 +126,7 @@ def group_board(request):
         if groupQuery:
             group_buying = Group_buying.objects.filter(title__contains = groupQuery)
             group_buying_list = Group_buying.objects.filter(title__contains =groupQuery).all()
-            paginator = Paginator(group_buying, 5)
+            paginator = Paginator(group_buying, 10)
             page = request.GET.get('page')
             posts = paginator.get_page(page)    #페이지 번호 받아 해당 페이지 리턴
                     # [2]
@@ -148,7 +148,7 @@ def group_board(request):
             group_buying_list = Group_buying.objects.all()
             group_buying_comment = Group_buying_comment.objects
             userinfo = User_info.objects
-            paginator = Paginator(group_buying_list, 5)
+            paginator = Paginator(group_buying_list, 10)
             page = request.GET.get('page')
             posts = paginator.get_page(page)    #페이지 번호 받아 해당 페이지 리턴
                     # [2]
@@ -186,7 +186,7 @@ def group_board_new(request, category):
 
         temp = Group_buying.objects.filter(category=category)
         group_buying_list = Group_buying.objects.filter(category = category).all()
-        paginator = Paginator(temp, 5)
+        paginator = Paginator(temp, 10)
         page = request.GET.get('page')
         posts = paginator.get_page(page)    #페이지 번호 받아 해당 페이지 리턴
                 # [2]
@@ -288,6 +288,7 @@ def Lcompany(request):
 
 def companyBuying(request):
     user_pk = request.session.get('user')
+    now = datetime.now()
     if user_pk:
         user = get_object_or_404(User, id = user_pk)
         if user.first_name == '사업자':
@@ -305,7 +306,7 @@ def companyBuying(request):
         if query2:
             companyBuying = Company_buying.objects.filter(title__contains=query2)
             companyinfo = Company.objects.all()
-            paginator = Paginator(companyBuying, 5)
+            paginator = Paginator(companyBuying, 20)
             page = request.GET.get('page')
             posts = paginator.get_page(page)    #페이지 번호 받아 해당 페이지 리턴
                     # [2]
@@ -321,12 +322,38 @@ def companyBuying(request):
             if end_index >= max_index:
                 end_index = max_index
             paginator_range = paginator.page_range[start_index:end_index]
-           
-            return render(request, 'Company_buying.html',{'apartment':apartment,'companyBuying':companyBuying, 'companyinfo':companyinfo, 'isCompany':isCompany, 'posts': posts, 'apartment':apartment,'paginator_range':paginator_range})
+
+
+            year = []
+            for i in posts:
+                year.append(i.finish_date.year)
+
+            month = []
+            for i in posts:
+                month.append(i.finish_date.month)
+
+            day = []
+            for i in posts:
+                day.append(i.finish_date.day)
+
+            d_day={}
+            for i in range(len(year)):
+                d_day[i]=int(relativedelta(datetime(year[i], month[i], day[i]), now).days)
+
+            post=[]
+            for i in posts:
+                post.append([i])
+            
+            for i in range(len(year)):
+                print(post[i])
+                post[i] += [d_day[i]]
+                print(post[i])
+            
+            return render(request, 'Company_buying.html',{'posts':posts,'apartment':apartment,'companyBuying':companyBuying, 'companyinfo':companyinfo, 'isCompany':isCompany, 'post': post, 'apartment':apartment,'paginator_range':paginator_range})
         else:
             companyBuying = Company_buying.objects.all()
             companyinfo = Company.objects.all()
-            paginator = Paginator(companyBuying, 5)
+            paginator = Paginator(companyBuying, 20)
             page = request.GET.get('page')
             posts = paginator.get_page(page)    #페이지 번호 받아 해당 페이지 리턴
                     # [2]
@@ -343,7 +370,33 @@ def companyBuying(request):
                 end_index = max_index
             paginator_range = paginator.page_range[start_index:end_index]
            
-            return render(request, 'Company_buying.html',{'apartment':apartment,'companyBuying':companyBuying, 'companyinfo':companyinfo, 'isCompany':isCompany, 'posts': posts, 'apartment':apartment,'paginator_range':paginator_range})
+           
+            year = []
+            for i in posts:
+                year.append(i.finish_date.year)
+
+            month = []
+            for i in posts:
+                month.append(i.finish_date.month)
+
+            day = []
+            for i in posts:
+                day.append(i.finish_date.day)
+
+            d_day={}
+            for i in range(len(year)):
+                d_day[i]=int(relativedelta(datetime(year[i], month[i], day[i]), now).days)
+
+            post=[]
+            for i in posts:
+                post.append([i])
+            
+            for i in range(len(year)):
+                print(post[i])
+                post[i] += [d_day[i]]
+                print(post[i])
+                
+            return render(request, 'Company_buying.html',{'apartment':apartment,'companyBuying':companyBuying, 'companyinfo':companyinfo, 'isCompany':isCompany,'post':post, 'posts': posts, 'apartment':apartment,'paginator_range':paginator_range})
         
     else:
         return redirect("/")
@@ -369,7 +422,7 @@ def fleaMarket(request):
         query=request.GET.get('search')
         if query:
             fleaMarket = Flee_market.objects.filter(title__contains=query)
-            paginator = Paginator(fleaMarket, 15)
+            paginator = Paginator(fleaMarket, 20)
             page = request.GET.get('page')
             posts = paginator.get_page(page)    #페이지 번호 받아 해당 페이지 리턴
                     # [2]
@@ -388,7 +441,7 @@ def fleaMarket(request):
             return render(request, 'fleaMarket.html', {'fleaMarket':fleaMarket, 'posts': posts, 'apartment':apartment, 'paginator_range':paginator_range, 'isUser':isUser})
         else:
             fleaMarket = Flee_market.objects.all()
-            paginator = Paginator(fleaMarket, 15)
+            paginator = Paginator(fleaMarket, 20)
             page = request.GET.get('page')
             posts = paginator.get_page(page)    #페이지 번호 받아 해당 페이지 리턴
                     # [2]
@@ -600,7 +653,7 @@ def fleaMarket_new(request,category):
             isUser = True
 
         temp=Flee_market.objects.filter(category=category)
-        paginator = Paginator(temp, 15)
+        paginator = Paginator(temp, 20)
         page = request.GET.get('page')
         posts = paginator.get_page(page)    #페이지 번호 받아 해당 페이지 리턴
                 # [2]
@@ -623,6 +676,7 @@ def fleaMarket_new(request,category):
 
 def companyBuying_new(request,category):
     user_pk = request.session.get('user')
+    now = datetime.now()
     if user_pk:
         user = get_object_or_404(User, id = user_pk)
         if user.first_name == '사업자':
@@ -636,7 +690,8 @@ def companyBuying_new(request,category):
             apartment = apartment.apartment
             isUser = True
         temp=Company_buying.objects.filter(category=category)
-        paginator = Paginator(temp, 5)
+        companyinfo = Company.objects.all()
+        paginator = Paginator(temp, 20)
         page = request.GET.get('page')
         posts = paginator.get_page(page)    #페이지 번호 받아 해당 페이지 리턴
                 # [2]
@@ -652,8 +707,34 @@ def companyBuying_new(request,category):
         if end_index >= max_index:
             end_index = max_index
         paginator_range = paginator.page_range[start_index:end_index]
+    
+    
+        year = []
+        for i in posts:
+            year.append(i.finish_date.year)
+
+        month = []
+        for i in posts:
+            month.append(i.finish_date.month)
+
+        day = []
+        for i in posts:
+            day.append(i.finish_date.day)
+
+        d_day={}
+        for i in range(len(year)):
+            d_day[i]=int(relativedelta(datetime(year[i], month[i], day[i]), now).days)
+
+        post=[]
+        for i in posts:
+            post.append([i])
         
-        return render(request, 'Company_buying.html',{'companyBuying':temp, 'posts': posts, 'apartment':apartment,'paginator_range':paginator_range, 'isUser':isUser})
+        for i in range(len(year)):
+            print(post[i])
+            post[i] += [d_day[i]]
+            print(post[i])
+        
+        return render(request, 'Company_buying.html',{'companyBuying':temp, 'companyinfo':companyinfo,'post':post,'posts': posts, 'apartment':apartment,'paginator_range':paginator_range, 'isUser':isUser})
 
 
     else:
